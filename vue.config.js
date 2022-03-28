10b468fee8d5bfee94f9a73be035b323498677cb
Aldo S <aldo11santiago@gmail.com>
2022-03-28 16:20:13 -0600

    build

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-Pasteleria-LaunchX-FrontendMission-Vuejs_Vuex/'
+    ? '/Pasteleria-LaunchX-FrontendMission-Vuejs_Vuex/'
     : '/'
 
 })
